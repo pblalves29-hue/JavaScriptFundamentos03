860e3238acc41f80f80b3f239d5da1e926adeaaa
Pablo Alves <pblalves29@gmail.com>
2026-08-11 16:43:11 -0300

    Atualizando projeto

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,14 @@
+function saudacao(){
+    console.log("Olá turma");
+}
+saudacao();
+
+function bomDia(){
+    console.log("bom dia")
+}
+bomDia();
+
+function apresentar(nome){
+    console.log(`Olá, ${nome}`);
+}
+apresentar("jefferson");
